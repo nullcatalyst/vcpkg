2c340e3255154ca20276b8636964d11b865f0cbf
Scott Bennett <scottbennett912@gmail.com>
2020-08-15 19:47:28 -0400

    Update chipmunk port build to use cmake instead of msbuild to support non-windows platforms

--- ports/chipmunk/portfile.cmake
+++ ports/chipmunk/portfile.cmake
@@ -1,25 +1,5 @@
 include(vcpkg_common_functions)
 
-#architecture detection
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-   set(CHIPMUNK_ARCH Win32)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-   set(CHIPMUNK_ARCH x64)
-else()
-   message(FATAL_ERROR "unsupported architecture")
-endif()
-
-#linking
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    set(CHIPMUNK_CONFIGURATION_SUFFIX " DLL")
-else()
-    if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
-        set(CHIPMUNK_CONFIGURATION_SUFFIX "")
-    else()
-        set(CHIPMUNK_CONFIGURATION_SUFFIX " SCRT")
-    endif()
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO slembcke/Chipmunk2D
@@ -28,48 +8,28 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/msvc/VS2015/chipmunk/chipmunk.vcxproj
-    RELEASE_CONFIGURATION "Release${CHIPMUNK_CONFIGURATION_SUFFIX}"
-    DEBUG_CONFIGURATION "Debug${CHIPMUNK_CONFIGURATION_SUFFIX}"
-)
-
-message(STATUS "Installing")
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.dll"
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-    )
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.dll"
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-    )
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(BUILD_SHARED OFF)
 else()
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-    )
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-    )
+    set(BUILD_SHARED ON)
 endif()
 
-file(INSTALL
-    "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.lib"
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-file(INSTALL
-    "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.lib"
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(INSTALL
-    ${SOURCE_PATH}/include/chipmunk
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_DEMOS=OFF
+        -DINSTALL_DEMOS=OFF
+        -DBUILD_SHARED=${BUILD_SHARED}
+        -DBUILD_STATIC=ON
+        -DINSTALL_STATIC=ON
 )
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/chipmunk RENAME copyright)
+vcpkg_install_cmake()
 
-message(STATUS "Installing done")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
